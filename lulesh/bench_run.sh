855b5e54e1d7a1cf320e19bfefdbdeacb512fbe0
Brandon Kammerdiener <bkammerd@uruguay-mclean-dev.eecs.utk.edu>
2020-12-15 10:38:18 -0500

    fix git being stupid

---
+++ lulesh/bench_run.sh
@@ -0,0 +1,8 @@
+input_tiny=" -s 220 -i 3 -r 11 -b 0 -c 64 -p"
+input_small=" -s 220 -i 12 -r 11 -b 0 -c 64 -p"
+input_medium="-s 520 -i 6 -r 11 -b 0 -c 64 -p"
+input_large=" -s 690 -i 3 -r 11 -b 0 -c 64 -p"
+
+function do_bench_run {
+    run_prepared_cmd
+}
